f327a5b20f171370659b1b55fe0bbade143f1a4a
Hadrien <ketsapiwiq@protonmail.com>
2021-09-26 20:56:44 +0200

    fix port 9090 => 5000 in identidock

--- kubernetes/TP3-monsterstack/boot.sh
+++ kubernetes/TP3-monsterstack/boot.sh
@@ -2,8 +2,8 @@
 set -e
 if [ "$CONTEXT" = 'DEV' ]; then
     echo "Running Development Server"
-    exec python3 "/app/monster_icon.py"
+    FLASK_ENV=development exec python3 "/app/monster_icon.py"
 else
     echo "Running Production Server"
-    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/monster_icon.py --callable app --stats 0.0.0.0:9191
+    exec uwsgi --http 0.0.0.0:5000 --wsgi-file /app/monster_icon.py --callable app --stats 0.0.0.0:9191
 fi
