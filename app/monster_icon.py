b3e644e9e1760c66df4cadb206e24f699c134d93
Elie Gavoty <eliegavoty@free.fr>
2022-06-28 13:35:34 +0200

    add domain name env var for imagebackend and redis

--- app/monster_icon.py
+++ app/monster_icon.py
@@ -5,8 +5,12 @@ import hashlib
 import redis
 import socket
 
+import os
+imagebackend_domain = os.environ['IMAGEBACKEND_DOMAIN']
+redis_domain = os.environ['REDIS_DOMAIN']
+
 app = Flask(__name__)
-redis_cache = redis.StrictRedis(host='redis', port=6379, socket_connect_timeout=2, socket_timeout=2, db=0)
+redis_cache = redis.StrictRedis(host=redis_domain, port=6379, socket_connect_timeout=2, socket_timeout=2, db=0)
 salt = "UNIQUE_SALT"
 default_name = 'John Doe'
 
@@ -23,8 +27,9 @@ def mainpage():
         name = request.form['name']
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+    hostname = socket.gethostname()
 
-    page = '''
+    page = f'''
         <html>
           <head>
             <title>Monster Icon</title>
@@ -48,8 +53,7 @@ def mainpage():
           </ul></strong>
         </body>
        </html>
-    '''.format(name=name, name_hash=name_hash, hostname=socket.gethostname(), visits=visits)
-
+    '''
     return page
 
 
@@ -58,17 +62,6 @@ def get_identicon(name):
     image = redis_cache.get(name)
     if image is None:
         print ("Cache miss: picture icon not found in Redis", flush=True)
-        r = requests.get('http://imagebackend:8080/monster/' + name + '?size=80')
+        r = requests.get(f"http://{imagebackend_domain}:8080/monster/{name}?size=80")
         image = r.content
     redis_cache.set(name, image)
-
-    return Response(image, mimetype='image/png')
-
-@app.route('/healthz')
-def healthz():
-    data = {'ready': 'true'}
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
